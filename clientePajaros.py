033274ee29c8544e1453c10f302a2b388f140e56
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-03-09 18:10:31 -0300

    configuraciones básicas y primera prueba de sockets

---
+++ clientePajaros.py
@@ -0,0 +1,13 @@
+import socket
+from time import sleep
+
+
+i=0
+while(i<10):
+    mi_socket = socket.socket()
+    mi_socket.connect(('192.168.1.38',8888))
+    mensaje=str(i)+" mensaje"
+    mi_socket.send(str.encode(mensaje))
+    mi_socket.close()
+    sleep(1)
+print("bye bye bro")
---
+++ servidorPajaros.py
@@ -0,0 +1,13 @@
+import socket
+
+mi_socket = socket.socket()
+mi_socket.bind(('192.168.1.38',8888))
+mi_socket.listen(2)
+while True:
+    conexion, addr = mi_socket.accept()
+    print('conexion entrante desde:'+ str(addr))
+    informacionEntrante = conexion.recv(1024)
+    print(str(informacionEntrante))
+
+
+
