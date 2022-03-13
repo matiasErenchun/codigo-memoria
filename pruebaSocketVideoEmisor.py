aab1827b029bd33c75e249a2a2e81f1032a234e7
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-03-13 16:35:53 -0300

    cambio  de archivos

--- pruebaSocketVideoEmisor.py
+++ pruebaSocketVideoEmisor.py
@@ -1,27 +1,35 @@
 import socket, cv2, pickle, struct
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = '192.168.1.38'
+# Socket Create
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_name = socket.gethostname()
+#host_ip = socket.gethostbyname(host_name)
+host_ip='192.168.1.38'
+print('HOST IP:', host_ip)
 port = 9999
-client_socket.connect((host_ip, port))
-data = b""
-payload_size = struct.calcsize("Q")
-while True:
-    while len(data) < payload_size:
-        packet = client_socket.recv(4 * 1024)  # 4K
-        if not packet: break
-        data += packet
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("Q", packed_msg_size)[0]
+socket_address = (host_ip, port)
 
-    while len(data) < msg_size:
-        data += client_socket.recv(4 * 1024)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    frame = pickle.loads(frame_data)
-    cv2.imshow("RECEIVING VIDEO", frame)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
-client_socket.close()
+# Socket Bind
+server_socket.bind(socket_address)
+
+# Socket Listen
+server_socket.listen(5)
+print("LISTENING AT:", socket_address)
+
+# Socket Accept
+while True:
+    client_socket, addr = server_socket.accept()
+    print('GOT CONNECTION FROM:', addr)
+    if client_socket:
+        vid = cv2.VideoCapture(0)
+
+        while (vid.isOpened()):
+            img, frame = vid.read()
+            a = pickle.dumps(frame)
+            message = struct.pack("Q", len(a)) + a
+            client_socket.sendall(message)
+
+            cv2.imshow('TRANSMITTING VIDEO', frame)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                client_socket.close()
