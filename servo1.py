16b6bc6a53beb7a6b463e8e515d250ab73a3b680
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-04-27 18:00:15 -0400

    prueba de giro

--- servo1.py
+++ servo1.py
@@ -3,16 +3,21 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setip(11,GPIO.OUT)
-servo1 = GPIO.PWM(11,50)
+GPIO.setip(11, GPIO.OUT)
+servo1 = GPIO.PWM(11, 50)
 
 servo1.start(0)
+i = 0
 try:
-    while True:
-        angulo = float(input('ingrese un angulo:'))
-        servo1.ChangeDutyCycle(2+(angulo/18))
+    while i <= 10:
+        if (i % 2 == 0):
+            angulo = 90
+        else:
+            angulo = 0
+        servo1.ChangeDutyCycle(2 + (angulo / 18))
         time.sleep(0.5)
         servo1.ChangeDutyCycle(0)
+        i += 1
 finally:
     servo1.stop()
     GPIO.cleanup()
