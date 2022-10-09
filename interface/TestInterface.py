ce95a5cb6d437838ae66464bbabb4049aac0f141
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-10-09 17:43:25 -0300

    inicio prueba de interfaz web

---
+++ interface/TestInterface.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Ventana de alerta rapaz")
+root.config(background="#FFFFFF")
+
+mainframe = Frame()
+
+mainframe.config(background="black", height="50", width="50")
+mainframe.pack()
+
+root.mainloop()
---
+++ interfaz/interfazraptordetector/src/views/RaptorDetected.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{name}} un ave paraz fue detectada id imagen:{{$route.params.id}}</h1>
+</template>
+
+<script>
+export default {
+  name: "RaptorDetected",
+  computed:
+      {
+        name()
+        {
+          return this.$route.query.name
+        }
+      }
+}
+</script>
+
+<style scoped>
+
+</style>
