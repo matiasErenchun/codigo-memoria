08aa32d9c936d091d0f31c126346d8e2d33ee6ae
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-10-14 17:42:03 -0300

    carga dinámica de imagen en interfaz básica

--- interfaz/interfazraptordetector/src/views/RaptorDetected.vue
+++ interfaz/interfazraptordetector/src/views/RaptorDetected.vue
@@ -1,15 +1,42 @@
 <template>
-  <h1>{{name}} un ave paraz fue detectada id imagen:{{$route.params.id}}</h1>
+  <div>
+    <h1>{{name}} un ave paraz fue detectada id imagen:{{$route.params.id}}</h1>
+    <img style="width: 80%" :src="img" alt="alt"/>
+  </div>
 </template>
 
 <script>
 export default {
   name: "RaptorDetected",
+  data: () =>({
+    img: "",
+    alt: ""
+  }),
   computed:
       {
         name()
         {
           return this.$route.query.name
+        }
+      },
+  created()
+  {
+    this.gentimagen()
+  },
+  methods:
+      {
+        async gentimagen()
+        {
+          const datos = await fetch("http://127.0.0.1:5000/imagen",{
+            method: 'POST',
+            headers: {
+              'id': this.$route.params.id
+            }
+          })
+          const respuesta = await datos.json()
+
+          console.log(respuesta.url)
+          this.img=respuesta.url
         }
       }
 }
