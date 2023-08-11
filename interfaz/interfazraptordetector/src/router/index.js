6d811fafe0388df618f401b455fbba35411997ab
Matias <matiaserenchun@gmai.com>
2023-08-10 20:43:06 -0400

    wip (new view all detections)

--- interfaz/interfazraptordetector/src/router/index.js
+++ interfaz/interfazraptordetector/src/router/index.js
@@ -19,6 +19,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/all_detections',
+    name: 'Todas las Deteccciones',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AllDetections.vue')
+  },
+  {
     path: '/:id',
     name: 'Rapaz Detectada',
     // route level code-splitting
@@ -26,6 +34,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/RaptorDetected.vue')
   }
+
 ]
 
 const router = new VueRouter({
---
+++ interfaz/interfazraptordetector/src/views/AllDetections.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <H1>Holaaaaaaa &lt;3</H1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
